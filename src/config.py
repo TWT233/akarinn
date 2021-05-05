494eb8b9b7557cf2d8cda8d5b92a9fae1a7e7f8e
TWT <twt2333@outlook.com>
2021-05-05 15:04:09 +0800

    feat(config): more getters

--- src/config.py
+++ src/config.py
@@ -1,11 +1,17 @@
+import random
+
 import yaml
 
 with open('conf/battle.yaml', 'r', encoding='utf-8') as f:
     battle_conf = yaml.load(f, Loader=yaml.FullLoader)
 
 
+def conf_battle() -> dict:
+    return battle_conf
+
+
 def conf_battle_stage() -> list:
-    return battle_conf['stages']
+    return conf_battle()['stages']
 
 
 def battle_stage_count() -> int:
@@ -22,8 +28,12 @@ with open('conf/system.yaml', 'r', encoding='utf-8') as f:
     system_conf = yaml.load(f, Loader=yaml.FullLoader)
 
 
+def conf_system() -> dict:
+    return system_conf
+
+
 def conf_system_db() -> dict:
-    return system_conf['db']
+    return conf_system()['db']
 
 
 def db_path() -> str:
@@ -31,4 +41,15 @@ def db_path() -> str:
 
 
 def conf_system_mode() -> str:
-    return system_conf['system']['mode']
+    return conf_system()['mode']
+
+
+if 'op_key' in conf_system():
+    op_key = conf_system()['op_key']
+else:
+    op_key = ''.join([random.choice('0123456789abcdef') for x in range(16)])
+    print(f'op_key: {op_key}')
+
+
+def get_op_key() -> str:
+    return op_key
